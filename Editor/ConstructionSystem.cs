99b5c64dc9151b27fe6946963eb479b25ef00570
Cnfnbcn <dolgopolovdaniildmitrievich@gmail.com>
2023-06-04 14:29:33 +0500

    renamed ConstructorSystem to ConstructionSystem

---
+++ Editor/ConstructionSystem.cs
@@ -0,0 +1,12 @@
+#if UNITY_EDITOR
+namespace JoanofArcGames.PackageConstructor
+{
+	public static class ConstructionSystem
+	{
+		public static void Construct(ConfigData config)
+		{
+			
+		}
+	}
+}
+#endif
