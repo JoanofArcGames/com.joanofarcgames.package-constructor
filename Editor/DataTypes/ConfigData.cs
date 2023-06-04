5a67977e3bdb49ebacbfeb0512e5c5a0c18f0957
Cnfnbcn <dolgopolovdaniildmitrievich@gmail.com>
2023-06-04 14:27:24 +0500

    structured data types into relevant folder

---
+++ Editor/DataTypes/ConfigData.cs
@@ -0,0 +1,9 @@
+#if UNITY_EDITOR
+namespace JoanofArcGames.PackageConstructor
+{
+	public struct ConfigData
+    {
+        
+    }
+}
+#endif
---
+++ Editor/DataTypes/Dependency.cs
@@ -0,0 +1,9 @@
+#if UNITY_EDITOR
+namespace JoanofArcGames.PackageConstructor
+{
+	public struct Dependency
+    {
+    	
+    }
+}
+#endif
